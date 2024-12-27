69eb44c1b69a2300ae251652c532c5166c34f27e
ali-rostom1 <ali.rostom220@gmail.com>
2024-12-27 01:42:53 +0100

    add insert method to dbcon class

--- src/pages/classes/dbcon.class.php
+++ src/pages/classes/dbcon.class.php
@@ -26,7 +26,7 @@
             $this->data = $this->data->fetchAll(PDO::FETCH_ASSOC);
             return $this->data;
         }
-        
+
         public function selectWhere(string $table,$columnName,$value,$valueType){
             $this->sql = "SELECT * FROM $table WHERE $columnName = ";
             if($valueType == "int"){
@@ -39,6 +39,45 @@
             return $this->data;
         }
         
+        public function Insert($tableName, $values){
+            switch ($tableName) {
+                case 'continent':
+                    $this->sql = "INSERT INTO continent(name) VALUES (";
+                    break;
+                
+                case 'pays':
+                    $this->sql = "INSERT INTO pays(nom,population,langues,id_continent) VALUES (";
+                    break;
+                
+                case 'ville':
+                    $this->sql = "INSERT INTO ville(nom, description, type, id_pays) VALUES (";
+                    break;
+
+                default:
+                    return 0;
+                    break;
+            }
+
+            foreach($values as $index=>$value){
+
+                if ($value['type'] == 'int' ) {
+                    
+                    $this->sql .= $value['val'];
+                } else if ($value['type'] == 'string') {
+                    $this->sql .= "'" . $value['val'] . "'";
+                }
+
+                if (isset($values[$index + 1]['val'])) {
+                    $this->sql .= ",";
+                }
+
+            }
+            $this->sql .= ");";
+
+            $this->data = self::connect()->query($this->sql);      
+
+
+        }
 
 
 
